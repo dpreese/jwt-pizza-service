a6aba2d851e6f21329d4996d8796825f94b66420
dpreese <derekpreese@gmail.com>
2024-11-08 12:17:00 -0700

    lint changhes for franchiseRouter.test.js

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,36 +1,36 @@
 const request = require('supertest');
 const app = require('../service');
 const jwt = require('jsonwebtoken');
-const config = require('../config.js');
-const { Role, DB } = require('../database/database.js');
+// const config = require('../config.js');
+// const { Role, DB } = require('../database/database.js');
 
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
+// function randomName() {
+//   return Math.random().toString(36).substring(2, 12);
+// }
 
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = randomName();
-  user.email = user.name + '@admin.com';
+// async function createAdminUser() {
+//   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+//   user.name = randomName();
+//   user.email = user.name + '@admin.com';
 
-  user = await DB.addUser(user);
-  return { ...user, password: 'toomanysecrets' };
-}
+//   user = await DB.addUser(user);
+//   return { ...user, password: 'toomanysecrets' };
+// }
 
-let adminToken;
-let franchiseeToken;
+// let adminToken;
+// let franchiseeToken;
 
 beforeEach(async () => {
   jest.restoreAllMocks(); // Restore mocks before each test to prevent test interference
 
   // Create an admin user and generate token
-  const adminUser = await createAdminUser();
-  adminToken = jwt.sign({ id: adminUser.id, roles: adminUser.roles }, config.jwtSecret);
+  // const adminUser = await createAdminUser();
+  // adminToken = jwt.sign({ id: adminUser.id, roles: adminUser.roles }, config.jwtSecret);
 
-  // Create a franchisee user and generate token
-  const franchiseeUser = { name: randomName(), email: randomName() + '@diner.com', password: 'password', roles: [{ role: Role.Diner }] };
-  const createdFranchisee = await DB.addUser(franchiseeUser);
-  franchiseeToken = jwt.sign({ id: createdFranchisee.id, roles: createdFranchisee.roles }, config.jwtSecret);
+  // // Create a franchisee user and generate token
+  // const franchiseeUser = { name: randomName(), email: randomName() + '@diner.com', password: 'password', roles: [{ role: Role.Diner }] };
+  // const createdFranchisee = await DB.addUser(franchiseeUser);
+  // franchiseeToken = jwt.sign({ id: createdFranchisee.id, roles: createdFranchisee.roles }, config.jwtSecret);
 
   // Mock jwt.verify to always succeed and set req.user
   jest.spyOn(jwt, 'verify').mockImplementation((token, secret, callback) => {
